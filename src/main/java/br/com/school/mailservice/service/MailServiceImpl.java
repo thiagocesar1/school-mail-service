0d768b7a190ea6b9c09d60ff0a8efd3ab84de316
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-07-05 20:24:14 -0300

    Estrutura basica para o envio.

--- src/main/java/br/com/school/mailservice/service/MailServiceImpl.java
+++ src/main/java/br/com/school/mailservice/service/MailServiceImpl.java
@@ -3,6 +3,8 @@ package br.com.school.mailservice.service;
 import br.com.school.mailservice.domain.entity.Mail;
 import br.com.school.mailservice.domain.repository.MailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -12,9 +14,20 @@ public class MailServiceImpl implements MailService{
     @Autowired
     private MailRepository mailRepository;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
     public void sendMail(Mail mail) {
         mail.setDateSanded(LocalDate.now());
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(mail.getMailTo());
+        message.setSubject(mail.getMailType().getSubject());
+        message.setText("E-mail de teste sendo enviado");
+
+        javaMailSender.send(message);
+
         mailRepository.save(mail);
     }
 }
