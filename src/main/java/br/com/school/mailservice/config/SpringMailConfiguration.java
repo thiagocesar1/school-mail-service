beba96beb90d63a03d0c0fcdeae44497afe919cc
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-07-06 18:32:24 -0300

    Inclusão do thymeleaf e configuração básica para criação dos templates de e-mail em HTML.

---
+++ src/main/java/br/com/school/mailservice/config/SpringMailConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.school.mailservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Collections;
+
+@Configuration
+public class SpringMailConfiguration {
+
+    @Bean
+    public ITemplateResolver thymeleafTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+}
