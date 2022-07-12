bf103d1b310efabc152df59a3420cb6db7d6eb43
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-07-12 20:42:34 -0300

    Remoção do thymeleaf do projeto

--- src/main/java/br/com/school/mailservice/config/SpringMailConfiguration.java
+++ src/main/java/br/com/school/mailservice/config/SpringMailConfiguration.java
@@ -1,43 +1,9 @@
 package br.com.school.mailservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
-import java.util.Collections;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringMailConfiguration {
 
-    @Bean
-    @Primary
-    public ITemplateResolver thymeleafTemplateResolver() {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("classpath:/templates/");
-        templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML");
-        templateResolver.setCharacterEncoding("UTF-8");
-        return templateResolver;
-    }
-
-    @Bean
-    public ResourceBundleMessageSource emailMessageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("mailMessages");
-        return messageSource;
-    }
-
-    @Bean
-    public SpringTemplateEngine thymeleafTemplateEngine(ITemplateResolver templateResolver) {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver);
-        templateEngine.setTemplateEngineMessageSource(emailMessageSource());
-        return templateEngine;
-    }
 }
