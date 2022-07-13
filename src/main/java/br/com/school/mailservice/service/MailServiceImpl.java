5366ce8164f64bc333a97c53f7677262c4aa726a
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-07-12 21:13:15 -0300

    Reinclusao do thymeleaf e ajuste nas configuracoes/template de email.

--- src/main/java/br/com/school/mailservice/service/MailServiceImpl.java
+++ src/main/java/br/com/school/mailservice/service/MailServiceImpl.java
@@ -3,11 +3,18 @@ package br.com.school.mailservice.service;
 import br.com.school.mailservice.domain.entity.Mail;
 import br.com.school.mailservice.domain.repository.MailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.Map;
 
 @Service
 public class MailServiceImpl implements MailService{
@@ -17,10 +24,30 @@ public class MailServiceImpl implements MailService{
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
+
     @Override
     public void sendMail(Mail mail) throws MessagingException {
         mail.setDateSanded(LocalDate.now());
-
+        sendEmail(mail.getMailTo(), mail.getMailType().getSubject(), mail.getAttributes());
         mailRepository.save(mail);
+    }
+
+    public void sendEmail(
+            String to, String subject, Map<String, String> templateModel) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+        Context context = new Context();
+        context.setVariable("name", templateModel.get("userName"));
+        context.setVariable("registerLink", templateModel.get("registerLink"));
+
+        String html = springTemplateEngine.process("lead-created-mail", context);
+        helper.setTo(to);
+        helper.setText(html, true);
+        helper.setSubject(subject);
+        javaMailSender.send(message);
     }
 }
