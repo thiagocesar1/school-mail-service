af4e40f9198ada7933c238dacf51d14e70bfcce3
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-07-14 20:38:31 -0300

    Adição da logo e estilização do e-mail inicial

--- src/main/java/br/com/school/mailservice/service/MailServiceImpl.java
+++ src/main/java/br/com/school/mailservice/service/MailServiceImpl.java
@@ -3,7 +3,9 @@ package br.com.school.mailservice.service;
 import br.com.school.mailservice.domain.entity.Mail;
 import br.com.school.mailservice.domain.repository.MailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,9 @@ public class MailServiceImpl implements MailService{
     @Autowired
     private SpringTemplateEngine springTemplateEngine;
 
+    @Value("classpath:/templates/logo.png")
+    private Resource resourceFile;
+
     @Override
     public void sendMail(Mail mail) throws MessagingException {
         mail.setDateSanded(LocalDate.now());
@@ -48,6 +53,7 @@ public class MailServiceImpl implements MailService{
         helper.setTo(to);
         helper.setText(html, true);
         helper.setSubject(subject);
+        helper.addInline("schoolapp.png", resourceFile);
         javaMailSender.send(message);
     }
 }
